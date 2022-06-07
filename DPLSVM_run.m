4d1a80c48b2a7549362d64f79f03aa4688413ca1
mingjin112 <jming2@emory.edu>
2022-06-07 15:21:43 -0400

    Update DPLSVM_run.m

--- DPLSVM_run.m
+++ DPLSVM_run.m
@@ -1,8 +1,14 @@
 %% load data
 load('DATA.mat')
-% Omega : input network information for subjects
-% scov  : input supplementary covariates for subjects
-% Ylabel: input label for subjects
+% Omega : N * Q matrix of network information
+% scov  : N * C matric of covariates 
+% Yi    : N * 1 matrix of label
+
+% N : total number of subjects
+% Q : total number of screened edges per subject
+% C : total number of covariates per subject
+% P : total number of varialbes (number of screened edges + number of covariates)
+
 
 %% run HCP_LR1
 
